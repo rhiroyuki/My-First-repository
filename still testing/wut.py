fd1d5f8f43ae461b48bf8eb166b9f361e6f751e8
ricardohiroyuki <ricardo.hiroyuki@Outlook.com>
2015-05-08 12:26:08 -0300

    Refeito funcao vowel em wut.py

--- still testing/wut.py
+++ still testing/wut.py
@@ -1,12 +1,8 @@
 import string
 
 def vowel(msg):
-    b = ''
-    for char in msg:
-        if char in 'aeiou':
-            b = b + char
-    return b
-
+    return ''.join([char for char in msg if char in 'aeiou'])
+    
 def consonant(msg):
     return ''.join([char for char in msg if char not in 'aeiou' \
                     and char not in string.punctuation])
